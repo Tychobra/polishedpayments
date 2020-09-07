b2303ad9700f5db0708922cdc9a51d2ca78eedd4
merlinoa <andy.merlino@tychobra.com>
2020-09-07 14:30:29 -0400

    example app links to sign out and go to payments

--- inst/examples/polished_payments_min/server.R
+++ inst/examples/polished_payments_min/server.R
@@ -2,6 +2,23 @@
 
 server <- function(input, output, session) {
 
+  observeEvent(input$sign_out, {
+
+    polished::sign_out_from_shiny()
+    session$reload()
+
+  })
+
+  observeEvent(input$go_to_payments, {
+    shiny::updateQueryString(
+      queryString = "?page=payments",
+      session = session,
+      mode = "replace"
+    )
+    session$reload()
+  })
+
+
   output$polished_user <- renderPrint({
     session$userData$user()
   })
--- inst/examples/polished_payments_min/ui.R
+++ inst/examples/polished_payments_min/ui.R
@@ -3,7 +3,25 @@ ui <- fluidPage(
   fluidRow(
     column(
       12,
-      h1("Hello Shiny!")
+
+      h1(
+        style = "display: inline-block;",
+        "Hello Shiny!"
+      ),
+      actionLink(
+        "sign_out",
+        "Sign Out",
+        icon = icon("sign-out"),
+        style = "display: inline-block; margin-top: 25px; margin-left: 20px;",
+        class = "pull-right"
+      ),
+      actionLink(
+        "go_to_payments",
+        "Payments",
+        icon = icon("credit-card"),
+        style = "display: inline-block; margin-top: 25px;",
+        class = "pull-right"
+      ),
     )
   ),
   fluidRow(
