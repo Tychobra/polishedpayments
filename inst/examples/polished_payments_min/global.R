a88f70fd9afb85811f45a6b224b87c7aae7a9797
merlinoa <andy.merlino@tychobra.com>
2020-09-09 15:51:40 -0400

    removed database connection

--- inst/examples/polished_payments_min/global.R
+++ inst/examples/polished_payments_min/global.R
@@ -2,7 +2,7 @@ library(shiny)
 library(polished)
 library(RPostgres)
 library(DBI)
-#library(polishedpayments)
+library(polishedpayments)
 
 app_config <- config::get()
 
@@ -11,16 +11,3 @@ global_sessions_config(
   app_name = "polished_payments_min",
   api_url = app_config$api_url
 )
-
-conn <- DBI::dbConnect(
-  RPostgres::Postgres(),
-  dbname = app_config$db$dbname,
-  host = app_config$db$host,
-  password = app_config$db$password,
-  user = app_config$db$user
-)
-
-onStop(function() {
-  DBI::dbDisconnect(conn)
-})
-
