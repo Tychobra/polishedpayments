f0d4cf51d208cfcda6ec3a3ea7c928dba802524a
merlinoa <andy.merlino@tychobra.com>
2021-03-27 13:37:13 -0400

    do not set the api_url in demo app pushed to GH

--- inst/examples/polished_payments_min/global.R
+++ inst/examples/polished_payments_min/global.R
@@ -18,6 +18,5 @@ polished_payments_config(
 global_sessions_config(
   api_key = app_config$api_key,
   app_name = "polished_payments_min",
-  api_url = app_config$api_url,
   is_invite_required = FALSE
 )
