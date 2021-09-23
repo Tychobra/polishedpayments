57e2bce4dc973dadb1ecd352491f5442aae5032c
merlinoa <andy.merlino@tychobra.com>
2021-09-23 14:14:22 -0400

    function to get the session$userData$stripe() data

--- R/api_stripe_customer.R
+++ R/api_stripe_customer.R
@@ -1,5 +1,109 @@
 
 
+get_stripe <- function(
+  user_uid,
+  user_roles,
+  is_on_payments = FALSE,
+  app_uid = getOption("polished")$app_uid,
+  stripe_prices = getOption("pp")$prices,
+  free_roles = getOption("pp")$free_roles
+) {
+  customer_res <- get_customers(
+    app_uid = app_uid,
+    user_uid = user_uid
+  )
+
+  if (!identical(httr::status_code(customer_res$response), 200L)) {
+    print(customer_res$content)
+    stop("error getting subscription from Polished API", call. = FALSE)
+  }
+
+  customer <- as.list(customer_res$content)
+
+
+
+  if (identical(nrow(customer), 0L)) {
+    stop("no customer", call. = FALSE)
+
+  } else if (is.null(stripe_prices)) {
+    # app is using single payments only (i.e. no subscriptions)
+
+    out <- list(
+      polished_customer_uid = customer$uid,
+      stripe_customer_id = customer$stripe_customer_id,
+      free_user = FALSE,
+      default_payment_method = customer$default_payment_method,
+      trial_days_remaining = customer$free_trial_days_remaining_at_cancel,
+      subscription = NA
+    )
+
+  } else if (length(intersect(user_roles, free_roles)) > 0) {
+    # app is using subscriptions, but user is allowed to access the app without a subscription
+    # because they are a "free_user"
+    out <- list(
+      polished_customer_uid = customer$uid,
+      stripe_customer_id = customer$stripe_customer_id,
+      free_user = TRUE,
+      default_payment_method = customer$default_payment_method,
+      trial_days_remaining = customer$free_trial_days_remaining_at_cancel,
+      subscription = NA
+    )
+
+  } else {
+
+    out <- list(
+      polished_customer_uid = customer$uid,
+      stripe_customer_id = customer$stripe_customer_id,
+      free_user = FALSE,
+      default_payment_method = customer$default_payment_method,
+      trial_days_remaining = customer$free_trial_days_remaining_at_cancel
+    )
+
+    if (is.na(customer$stripe_subscription_id)) {
+      # customer does not have a subscription or trial and one is required.  Since user is on app, redirect to payments.
+      if (isFALSE(is_on_payments)) {
+
+        out <- NULL
+
+      } else {
+        stripe_out$subscription <- NA
+      }
+
+
+    } else {
+      stripe_sub <- get_stripe_subscription(customer$stripe_subscription_id)
+
+      # if subscription is NA, that means the user has canceled their subscription, so redirect them to the
+      # account page for them to restart their subscription
+
+      if ((stripe_sub$trial_days_remaining <= 0 && is.na(customer$default_payment_method)) && isFALSE(is_on_payments)) {
+
+        print("subscription has been cancelled")
+        out <- NULL
+      } else {
+
+        out$trial_days_remaining <- stripe_sub$trial_days_remaining
+
+        out$subscription = list(
+          uid = customer$uid,
+          stripe_subscription_id = customer$stripe_subscription_id,
+          item_id = stripe_sub$item_id,
+          plan_id = stripe_sub$plan_id,
+          nickname = stripe_sub$nickname,
+          amount = stripe_sub$amount,
+          interval = stripe_sub$interval,
+          trial_end = stripe_sub$trial_end,
+          created_at = customer$created_at
+        )
+      }
+
+    }
+  }
+
+  return(out)
+}
+
+
 #' Create a new Stripe customer
 #'
 #' @param email The new Stripe customer's email address.
--- R/payments_server.R
+++ R/payments_server.R
@@ -32,110 +32,25 @@ payments_server <- function(
 
       tryCatch({
         # get any existing subscriptions from Polished API
-        customer_res <- get_customers(
-          app_uid = getOption("polished")$app_uid,
-          user_uid = session$userData$user()$user_uid
+
+        stripe_out <- get_stripe(
+          user_uid = hold_user$user_uid,
+          user_roles = hold_user$roles,
+          is_on_payments = identical(payments_query, "TRUE")
         )
 
-        if (!identical(httr::status_code(customer_res$response), 200L)) {
-          print(customer_res$content)
-          stop("error getting subscription from Polished API", call. = FALSE)
-        }
-
-        customer <- as.list(customer_res$content)
-
-
-
-        if (identical(nrow(customer), 0L)) {
-          stop("no customer", call. = FALSE)
-
-        } else if (is.null(getOption("pp")$prices)) {
-          # app is using single payments only (i.e. no subscriptions)
-
-          session$userData$stripe(list(
-            polished_customer_uid = customer$uid,
-            stripe_customer_id = customer$stripe_customer_id,
-            free_user = FALSE,
-            default_payment_method = customer$default_payment_method,
-            trial_days_remaining = customer$free_trial_days_remaining_at_cancel,
-            subscription = NA
-          ))
-          return()
-
-        } else if (length(intersect(hold_user$roles, getOption("pp")$free_roles)) > 0) {
-          # app is using subscriptions, but user is allowed to access the app without a subscription
-          # because they are a "free_user"
-          session$userData$stripe(list(
-            polished_customer_uid = customer$uid,
-            stripe_customer_id = customer$stripe_customer_id,
-            free_user = TRUE,
-            default_payment_method = customer$default_payment_method,
-            trial_days_remaining = customer$free_trial_days_remaining_at_cancel,
-            subscription = NA
-          ))
-          return()
-
-        } else {
-
-          stripe_out <- list(
-            polished_customer_uid = customer$uid,
-            stripe_customer_id = customer$stripe_customer_id,
-            free_user = FALSE,
-            default_payment_method = customer$default_payment_method,
-            trial_days_remaining = customer$free_trial_days_remaining_at_cancel
+        if (is.null(stripe_out)) {
+          shiny::updateQueryString(
+            queryString = "?payments=TRUE",
+            session = session,
+            mode = "replace"
           )
-
-          if (is.na(customer$stripe_subscription_id)) {
-            # customer does not have a subscription or trial and one is required.  Since user is on app, redirect to payments.
-            if (!identical(payments_query, "TRUE")) {
-              shiny::updateQueryString(
-                queryString = "?payments=TRUE",
-                session = session,
-                mode = "replace"
-              )
-              session$reload()
-              return()
-            } else {
-              stripe_out$subscription <- NA
-            }
-
-
-          } else {
-            stripe_sub <- get_stripe_subscription(customer$stripe_subscription_id)
-
-            # if subscription is NA, that means the user has canceled their subscription, so redirect them to the
-            # account page for them to restart their subscription
-            # user is either in trial period or they already have a subscription with
-            # billing enabled, so let them access the app.
-            if ((stripe_sub$trial_days_remaining <= 0 && is.na(customer$default_payment_method)) && !identical(payments_query, "TRUE")) {
-              shiny::updateQueryString(
-                queryString = "?payments=TRUE",
-                session = session,
-                mode = "replace"
-              )
-              session$reload()
-            } else {
-
-              stripe_out$trial_days_remaining <- stripe_sub$trial_days_remaining
-
-              stripe_out$subscription = list(
-                uid = customer$uid,
-                stripe_subscription_id = customer$stripe_subscription_id,
-                item_id = stripe_sub$item_id,
-                plan_id = stripe_sub$plan_id,
-                nickname = stripe_sub$nickname,
-                amount = stripe_sub$amount,
-                interval = stripe_sub$interval,
-                trial_end = stripe_sub$trial_end,
-                created_at = customer$created_at
-              )
-            }
-
-          }
-
-
+          session$reload()
+        } else {
           session$userData$stripe(stripe_out)
         }
+
+
 
       }, error = function(err) {
 
