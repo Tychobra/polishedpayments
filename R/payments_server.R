8eeb5d9e404048000d6f140fa1761d9f0d33439d
merlinoa <andy.merlino@tychobra.com>
2022-05-03 06:07:48 -0400

    updated logic to access polishedpayments/shiny/sign_in app

--- R/payments_server.R
+++ R/payments_server.R
@@ -22,7 +22,6 @@ payments_server <- function(
   function(input, output, session) {
 
     session$userData$stripe <- reactiveVal(NULL)
-    session$userData$stripe_trigger <- reactiveVal(0)
     shiny::observeEvent(session$userData$user(), {
 
       query_list <- shiny::getQueryString()
@@ -31,74 +30,101 @@ payments_server <- function(
 
       hold_user <- session$userData$user()
       is_on_payments <- identical(query_page, "payments")
-      tryCatch({
-        # get any existing subscriptions from Polished API
 
-        stripe_out <- get_stripe(
-          user_uid = hold_user$user_uid
-        )
+      browser()
+      stripe_out <- NULL
+      if (!is.null(hold_user)) {
+        tryCatch({
+          # get any existing subscriptions from Polished API
+
+          stripe_out <- get_stripe(
+            user_uid = hold_user$user_uid
+          )
+
+        }, error = function(err) {
+
+          print(err)
+
+          if (identical(err$message, "subscription canceled")) {
+            # you can't create a Stripe subscription until after the Stripe user
+            # has entered their payment info.  If the stripe user needs to create
+            # a subscription, send them to the payments page.
+            shiny::updateQueryString(
+              queryString = "?page=payments",
+              session = session,
+              mode = "replace"
+            )
+            session$reload()
+          } else {
+
+            shinyFeedback::showToast("error", err$message)
+          }
+
+          invisible(NULL)
+        })
+      }
 
 
+      if (is_on_payments) {
 
-        if (!is_on_payments && isTRUE(.pp$is_subscription_required) && !is_subscription_valid(stripe_out)) {
-
+        if (is.null(user)) {
+          # go to registration.  User must register before they can create subscription
           shiny::updateQueryString(
-            queryString = "?page=payments",
+            queryString = "?page=sign_in&register=TRUE",
             session = session,
             mode = "replace"
           )
           session$reload()
 
         } else {
+          # go to polished payments
           session$userData$stripe(stripe_out)
+          payments_app_server(input, output, session)
         }
 
-
-      }, error = function(err) {
-
-        print(err)
-
-        if (identical(err$message, "subscription canceled")) {
-          # you can't create a Stripe subscription until after the Stripe user
-          # has entered their payment info.  If the stripe user needs to create
-          # a subscription, send them to the payments page.
-          shiny::updateQueryString(
-            queryString = "?page=payments",
-            session = session,
-            mode = "replace"
-          )
-          session$reload()
-        } else {
-
-          shinyFeedback::showToast("error", err$message)
-        }
-
-        invisible(NULL)
-      })
-
-    })
-
-
-
-    if (isTRUE(.pp$is_subscription_required)) {
-      ignore_null <- TRUE
-    } else {
-      ignore_null <- FALSE
-    }
-    observeEvent(session$userData$stripe(), {
-      query_list <- shiny::getQueryString()
-      page_query <- query_list$page
-
-      if (identical(page_query, "payments")) {
-
-        payments_app_server(input, output, session)
 
       } else {
 
-        server(input, output, session)
+        if (isTRUE(.pp$is_subscription_required)) {
+
+          if (is.null(user)) {
+            # redirect to registration page
+            shiny::updateQueryString(
+              queryString = "?page=sign_in&register=TRUE",
+              session = session,
+              mode = "replace"
+            )
+            session$reload()
+
+          } else if (is_subscription_valid(stripe_user)) {
+
+            # Go to custom Shiny app using polishedpayments
+            session$userData$stripe(stripe_out)
+            server(input, output, session)
+
+          } else {
+
+            shiny::updateQueryString(
+              queryString = "?page=payments",
+              session = session,
+              mode = "replace"
+            )
+            session$reload()
+
+          }
+
+        } else {
+
+          # subscription is not required
+          # Go to custom Shiny app using polishedpayments
+          session$userData$stripe(stripe_out)
+          server(input, output, session)
+
+        }
+
       }
 
-    }, once = TRUE, ignoreNULL = ignore_null)
+    }, ignoreNULL = polished::.polished$is_auth_required)
 
   }
 }
--- R/payments_ui.R
+++ R/payments_ui.R
@@ -15,138 +15,150 @@ payments_ui <- function(
 
   function(request) {
 
+    # TODO: should pass stripe wih request (i.e. `request$stripe`) like we do with polished user
     if (is.function(ui)) {
       ui <- ui(request)
     }
     ui <- force(ui)
 
-    user <- request$polished_user
-
-    if (is.null(user) && isTRUE(.pp$is_subscription_required)) {
-      stop("not authorized", call. = FALSE)
-    }
-
     query <- shiny::parseQueryString(request$QUERY_STRING)
     page_query <- query$page
 
-
-    err_out <- NULL
-    tryCatch({
-      # get existing subscriptions from Polished API
-
-      customer_res <- get_customers(
-        app_uid = polished::.polished$app_uid,
-        user_uid = user$user_uid
-      )
-
-
-      if (!identical(httr::status_code(customer_res$response), 200L)) {
-        stop(customer_res$content, call. = FALSE)
-      }
-
-      customer <- customer_res$content
-
-      if (identical(nrow(customer), 0L)) {
-
-        # This is the first time the user is accessing the app using polishedpayments, so
-        # set the user up with the default subscription
-
-        # Step 1: create Stripe customer
-
-        stripe_customer_id <- create_stripe_customer(
-          email = user$email,
-          user_uid = user$user_uid
-        )
+    user <- request$polished_user
+    stripe_user <- NULL
 
 
 
-
-        if (!is.null(.pp$prices) && .pp$trial_period_days > 0) {
-          # Step 2: If the app is using polishedpayments subscriptions, create the Stripe subscription on Stripe.
-          # If the subscription does not have a trial period, then we can't create the subscription until a payment
-          # method is enabled, so also do not create the subscription if no trial period.
-          stripe_subscription_id <- create_stripe_subscription(
-            stripe_customer_id,
-            plan_to_enable = .pp$prices[1],
-            days_remaining = .pp$trial_period_days
-          )
-        } else {
-          stripe_subscription_id <- NULL
-        }
-
-        # Step 3: add the newly created Stripe customer + possible subscription to the "customers" table
-        add_customer_res <- add_customer(
-          app_uid = polished::.polished$app_uid,
-          user_uid = user$user_uid,
-          stripe_customer_id = stripe_customer_id,
-          stripe_subscription_id = stripe_subscription_id
-        )
-
-        if (!identical(httr::status_code(add_customer_res$response), 200L)) {
-          print(add_customer_res$content)
-          stop("Polished API error creating customer", call. = FALSE)
-        }
+    if (!is.null(user)) {
+      err_out <- NULL
+      tryCatch({
+        # get existing subscriptions from Polished API
 
         customer_res <- get_customers(
           app_uid = polished::.polished$app_uid,
           user_uid = user$user_uid
         )
 
+
+        if (!identical(httr::status_code(customer_res$response), 200L)) {
+          stop(customer_res$content, call. = FALSE)
+        }
+
         customer <- customer_res$content
 
+        if (identical(nrow(customer), 0L)) {
+
+          # This is the first time the user is accessing the app using polishedpayments, so
+          # set the user up with the default subscription
+
+          # Step 1: create Stripe customer
+
+          stripe_customer_id <- create_stripe_customer(
+            email = user$email,
+            user_uid = user$user_uid
+          )
+
+
+
+
+          if (!is.null(.pp$prices) && .pp$trial_period_days > 0) {
+            # Step 2: If the app is using polishedpayments subscriptions, create the Stripe subscription on Stripe.
+            # If the subscription does not have a trial period, then we can't create the subscription until a payment
+            # method is enabled, so also do not create the subscription if no trial period.
+            stripe_subscription_id <- create_stripe_subscription(
+              stripe_customer_id,
+              plan_to_enable = .pp$prices[1],
+              days_remaining = .pp$trial_period_days
+            )
+          } else {
+            stripe_subscription_id <- NULL
+          }
+
+          # Step 3: add the newly created Stripe customer + possible subscription to the "customers" table
+          add_customer_res <- add_customer(
+            app_uid = polished::.polished$app_uid,
+            user_uid = user$user_uid,
+            stripe_customer_id = stripe_customer_id,
+            stripe_subscription_id = stripe_subscription_id
+          )
+
+          if (!identical(httr::status_code(add_customer_res$response), 200L)) {
+            print(add_customer_res$content)
+            stop("Polished API error creating customer", call. = FALSE)
+          }
+
+          customer_res <- get_customers(
+            app_uid = polished::.polished$app_uid,
+            user_uid = user$user_uid
+          )
+
+          customer <- customer_res$content
+
+        }
+
+
+        stripe_user <- get_stripe(
+          user_uid = user$user_uid
+        )
+
+      }, error = function(err) {
+
+        # if (identical(err$message, "subscription canceled")) {
+        #   update_customer(
+        #     customer_uid = customer$uid,
+        #     cancel_subscription = TRUE,
+        #     free_trial_days_remaining_at_cancel = 0
+        #   )
+        #   customer$stripe_subscription_id <- NA
+        # }
+
+        msg <- "unable to load UI"
+        print(msg)
+        print(err)
+        err_out <<- err$message
+
+        invisible(NULL)
+      })
+
+      if (!is.null(err_out)) {
+        return(tags$h1("Network connection error.  Please try again."))
       }
-
-
-      stripe_user <- get_stripe(
-        user_uid = user$user_uid
-      )
-
-    }, error = function(err) {
-
-      # if (identical(err$message, "subscription canceled")) {
-      #   update_customer(
-      #     customer_uid = customer$uid,
-      #     cancel_subscription = TRUE,
-      #     free_trial_days_remaining_at_cancel = 0
-      #   )
-      #   customer$stripe_subscription_id <- NA
-      # }
-
-      msg <- "unable to load UI"
-      print(msg)
-      print(err)
-      err_out <<- err$message
-
-      invisible(NULL)
-    })
-
-    if (!is.null(err_out)) {
-      return(tags$h1("Network connection error.  Please try again."))
     }
+
 
 
     if (identical(page_query, "payments")) {
 
-      out <- payments_app_ui(
-        app_name = app_name
-      )
+      if (is.null(user)) {
+        # TODO: redirect to registration page
+        out <- tags$h1("Redirecting to registration page")
+      } else {
+        # continue to payments page
+        out <- payments_app_ui(
+          app_name = app_name
+        )
+      }
 
     } else {
 
       if (isTRUE(.pp$is_subscription_required)) {
-        if (is_subscription_valid(stripe_user)) {
-          # Go to custom Shiny app using polishedpayments
-          out <- htmltools::tagList(
-            htmltools::tags$head(
-              tags$script(src = "https://js.stripe.com/v3"),
-              tags$script(paste0("var stripe = Stripe('", .pp$keys$public, "');")),
-              tags$script(src = "polishedpayments/js/polishedpayments.js?version=3")
-            ),
-            ui
-          )
+
+        if (is.null(user)) {
+          # redirect to registration page
+          out <- tags$h1("Redirecting to registration page")
+        } else if (is_subscription_valid(stripe_user)) {
+            # Go to custom Shiny app using polishedpayments
+            out <- htmltools::tagList(
+              htmltools::tags$head(
+                tags$script(src = "https://js.stripe.com/v3"),
+                tags$script(paste0("var stripe = Stripe('", .pp$keys$public, "');")),
+                tags$script(src = "polishedpayments/js/polishedpayments.js?version=3")
+              ),
+              ui
+            )
         } else {
           # Not Authorized - No subscription found - Redirecting to Payments
-          out <- NULL
+          out <- tags$h1("Redirecting to payments page")
         }
 
       }  else {
